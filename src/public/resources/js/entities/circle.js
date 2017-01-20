c66856d92c9551346985e7f7f59f3fd5f5b9e8d4
Hilmar Tryggvason <hilmart@gmail.com>
2017-01-20 18:59:58 +0000

    Reworked circle to use arc instead of many lines

--- src/public/resources/js/entities/circle.js
+++ src/public/resources/js/entities/circle.js
@@ -16,20 +16,50 @@ class Circle extends Shape {
         this.centerY = this.y + this.radiusY;
     }
 
+    move(deltaX, deltaY) {
+        this.x += deltaX;
+        this.y += deltaY;
+
+        this.endX = deltaX;
+        this.endY = deltaY;
+
+        this.centerX = deltaX;
+        this.centerY = deltaY;
+    }
+
     draw(context) {
 
-        // Reference: K3N @ StackOverflow (http://stackoverflow.com/users/1693593/k3n)
+        // Reference: Richard @ StackOverflow (http://stackoverflow.com/users/4506995/richard)
         // http://stackoverflow.com/questions/21594756/drawing-circle-ellipse-on-html5-canvas-using-mouse-events
 
+        //Save
+        context.save();
         context.beginPath();
-        context.moveTo(this.endX, this.centerY);
 
-        // Draw a lot of lines in a circle
-        for (var i = this.step; i < Math.PI * 2; i += this.step) {
-            context.lineTo(this.centerX + (this.radiusX * Math.cos(i)),
-                this.centerY + (this.radiusY * Math.sin(i)));
-        }
+        //Dynamic scaling
+        var scalex = 1 * ((this.endX - this.x) / 2);
+        var scaley = 1 * ((this.endY - this.y) / 2);
+        context.scale(scalex, scaley);
 
+        //Create ellipse
+        var centerx = (this.x / scalex) + 1;
+        var centery = (this.y / scaley) + 1;
+        context.arc(centerx, centery, 1, 0, 2 * Math.PI);
+
+        //Restore and draw
+        context.restore();
         context.stroke();
+    }
+
+    intersects(rect) {
+        var distX = Math.abs(this.centerX - (rect.x1 - rect.width / 2));
+        var distY = Math.abs(this.centerY - (rect.y1 - rect.height / 2));
+
+        if (distX > (rect.width / 2 + circle.r)) {
+            return false;
+        }
+        if (distY > (rect.height / 2 + circle.r)) {
+            return false;
+        }
     }
 }
